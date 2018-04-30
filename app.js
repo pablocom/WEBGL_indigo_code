afa34d0a52c833a7378cb44f6a8c2addbd084173
pablocom <p.c.ramirez5150@gmail.com>
2018-04-30 17:31:24 +0200

    cubo rotando acabado

--- app.js
+++ app.js
@@ -24,10 +24,13 @@ var fragmentShaderText = [
 	'}'
 ].join('\n');
 
+//para ahorrar cpu
+var gl;
+
 var InitDemo = function(){
 	console.log('this is motherfucking working');
 	var canvas = document.getElementById('game-surface');
-	var gl = canvas.getContext('webgl');
+	gl = canvas.getContext('webgl');
 
 	if(!gl){
 		console.log('WebGL not supported, downgraded to a experimental version of it.');
@@ -44,6 +47,12 @@ var InitDemo = function(){
 	// el color buffer dice el color de los puntos que tienes que pintar
 	// el depth buffer es una forma de ordenar en profundidad los objetos que vamos a pintar
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	gl.enable(gl.DEPTH_TEST);
+	gl.enable(gl.CULL_FACE);
+	gl.frontFace(gl.CCW);
+	gl.cullFace(gl.BACK);
+
+
 
 	var vertexShader = gl.createShader(gl.VERTEX_SHADER);
 	var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -80,17 +89,79 @@ var InitDemo = function(){
 
 	// ahora tenemos que decirle a la tarjeta gráfica cuales son los 
 	// vertices que vamos a querer dibujar, con los colores que queremos en cada vertice
-	var triangleVertices= 
+	var boxVertices= 
 	[	// X Y Z         R    G    B
-		 0.0, 0.5,0.0,  1.0, 1.0, 0.0,
-		-0.5,-0.5,0.0,  0.7, 0.0, 1.0,
-		 0.5,-0.5,0.0,  0.1, 1.0, 0.6
+		// Top
+		-1.0, 1.0, -1.0,   0.5, 0.5, 0.5,
+		-1.0, 1.0, 1.0,    0.5, 0.5, 0.5,
+		1.0, 1.0, 1.0,     0.5, 0.5, 0.5,
+		1.0, 1.0, -1.0,    0.5, 0.5, 0.5,
+
+		// Left
+		-1.0, 1.0, 1.0,    0.75, 0.25, 0.5,
+		-1.0, -1.0, 1.0,   0.75, 0.25, 0.5,
+		-1.0, -1.0, -1.0,  0.75, 0.25, 0.5,
+		-1.0, 1.0, -1.0,   0.75, 0.25, 0.5,
+
+		// Right
+		1.0, 1.0, 1.0,    0.25, 0.25, 0.75,
+		1.0, -1.0, 1.0,   0.25, 0.25, 0.75,
+		1.0, -1.0, -1.0,  0.25, 0.25, 0.75,
+		1.0, 1.0, -1.0,   0.25, 0.25, 0.75,
+
+		// Front
+		1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
+		1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
+		-1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
+		-1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
+
+		// Back
+		1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
+		1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
+		-1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
+		-1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
+
+		// Bottom
+		-1.0, -1.0, -1.0,   0.5, 0.5, 1.0,
+		-1.0, -1.0, 1.0,    0.5, 0.5, 1.0,
+		1.0, -1.0, 1.0,     0.5, 0.5, 1.0,
+		1.0, -1.0, -1.0,    0.5, 0.5, 1.0
+	];
+	var boxIndices =
+	[
+		// Top
+		0, 1, 2,
+		0, 2, 3,
+
+		// Left
+		5, 4, 6,
+		6, 4, 7,
+
+		// Right
+		8, 9, 10,
+		8, 10, 11,
+
+		// Front
+		13, 12, 14,
+		15, 14, 12,
+
+		// Back
+		16, 17, 18,
+		16, 18, 19,
+
+		// Bottom
+		21, 20, 22,
+		22, 20, 23
 	];
 	// esto de abajo le esta diciendo que estos valores van a pasar a la tarjeta gráfica
-	var triangleVertexBufferObject = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
+	var boxVertexBufferObject = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
 	// 1, el tipo de buffer, 2 hay que pasar a float de 32 
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVertices), gl.STATIC_DRAW);
+
+	var boxIndexBufferObject = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
 
 	// despues de esto tenemos que pasar las variables al vertex shader
 	var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
@@ -126,27 +197,25 @@ var InitDemo = function(){
 	var worldMatrix = new Float32Array(16);
 	var viewMatrix = new Float32Array(16);
 	var projMatrix = new Float32Array(16);
-
 	mat4.identity(worldMatrix);
-
-	mat4.lookAt(viewMatrix, [0,0,-2], [0,0,0], [0,1,0]); 
+	mat4.lookAt(viewMatrix, [0,0,-8], [0,0,0], [0,1,0]); 
 	// 1- donde esta el que mira (ORIGEN)
 	// 2- a donde mira (VECTOR DESINO)
 	// 3- vector unitario ¿? (ni idea...)
-
 	mat4.perspective(projMatrix, glMatrix.toRadian(45), canvas.width / canvas.height, 0.1, 1000.0);
 	// 1 la salida por callback
 	// 2- el campo de vision vertical
 	// 3- Aspect ratio ¿?
 	// extremo de cercania
-	// extremo de lejania ¿? creo
-	
+	// extremo de lejania ¿? creoç
 
 	// seteamos todas a la identidad
-	
 	gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
 	gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
 	gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
+
+	var xRotationMatrix = new Float32Array(16);
+	var yRotationMatrix = new Float32Array(16);
 
 	//Main render loop
 	// esto se encarga de mover la escena y actualizarla para formar los fotogramas
@@ -155,13 +224,15 @@ var InitDemo = function(){
 	var angle = 0;
 	var loop = function(){
 		angle = performance.now() / 1000 / 6 * 2 * Math.PI;
-		mat4.rotate(worldMatrix, identityMatrix, angle, [0,1,0]); // el ultimo parametro quiere decir que será en el eje de la Y
+		mat4.rotate(yRotationMatrix, identityMatrix, angle, [0,1,0]); // el ultimo parametro quiere decir que será en el eje de la Y
+		mat4.rotate(xRotationMatrix, identityMatrix, angle / 4, [1,0,0]); // el ultimo parametro quiere decir que será en el eje de la Y
+		mat4.mul(worldMatrix, yRotationMatrix, xRotationMatrix); //multiplicamos las rotaciones y se las asignamos a la WorldMatrix
 		gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
 		// esto es necesario siempre que queramos actualizar variables de los shaders
 
 		gl.clearColor(0.75,0.85,0.8,1.0);
 		gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
-		gl.drawArrays(gl.TRIANGLES, 0, 3);
+		gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
 		requestAnimationFrame(loop);
 	};
 	requestAnimationFrame(loop);
